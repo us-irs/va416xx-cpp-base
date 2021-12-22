31ce02b3c4216c9669c8393985d79d795cb8f595
Robin Mueller <robin.mueller.m@gmail.com>
2021-12-22 10:10:57 +0100

    eclipse jlink debug working properly

--- jlink-gdb.sh
+++ jlink-gdb.sh
@@ -2,4 +2,4 @@
 # Start the JLinkGDBServer while also specifying the JLinkScript file. The JLinkScript is necessary
 # to disable ROM protection to allow flashing
 JLinkGDBServer -select USB -device Cortex-M4 -endian little -if SWD -speed 2000 \
-  -LocalhostOnly -vd -jlinkscriptfile ./jlink/JLinkSettings.JLinkScript
+  -LocalhostOnly -vd -jlinkscriptfile ./jlink/JLinkSettings.JLinkScript -log test.txt
